aede6c88a785023df234b92c48fa7669bcd93966
Divyansh Garg <divyanshgarg844@gmail.com>
2025-12-18 18:46:08 +0530

    Added Pdf ganeration functionality

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
-import { register ,login,uploadProfilePicture,updateUserProfile, getUserAndProfile, updateProfileData} from "../controllers/user.controller.js";
+import { register ,login,uploadProfilePicture,updateUserProfile, getUserAndProfile, updateProfileData, getAllUserProfiles, downloadProfile} from "../controllers/user.controller.js";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -23,8 +23,10 @@ router.route("/update_profile_picture")
 .post(upload.single('profile_picture'),uploadProfilePicture);
 
 router.route("/user_update").post(updateUserProfile);
-router.route("/get_user_and_profile").get(getUserAndProfile); 
+router.route("/get_user_and_profile").post(getUserAndProfile); 
 router.route("/update_profile_data").post(updateProfileData);
+router.route("/user/get_all_users").get(getAllUserProfiles);
+router.route("/user/download_resume").get(downloadProfile);
 
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(postRoutes);
-app.use(userRoutes);
-
+// Make sure routes are registered
+app.use('/', userRoutes);  // Add explicit prefix
+app.use('/', postRoutes);
+app.use(express.static('uploads'));
 
 const start = async () => {
-
     const connectDb = await mongoose.connect("mongodb+srv://divyanshgarg844_db_user:Divyansh%4087@socialmedia.1huoxaq.mongodb.net/?appName=SocialMedia")
-
+    
     app.listen(9080, () => {
         console.log("Server is running on port 9080");
     })
-
 }
 start();
