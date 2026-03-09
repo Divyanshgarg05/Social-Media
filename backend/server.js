75a23ee5e97b24f48c6fc24417cd5a21e029b3e5
Divyansh Garg <divyanshgarg844@gmail.com>
2026-03-09 17:10:43 +0530

    Updated env file

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const dbUrl = process.env.MONGO_URL;
+
 // Make sure routes are registered
 app.use('/', userRoutes);  // Add explicit prefix
 app.use('/', postRoutes);
 app.use(express.static('uploads'));
 
 const start = async () => {
-    const connectDb = await mongoose.connect("mongodb+srv://divyanshgarg844_db_user:Divyansh%4087@socialmedia.1huoxaq.mongodb.net/?appName=SocialMedia")
+    const connectDb = await mongoose.connect(dbUrl)
     
     app.listen(9080, () => {
         console.log("Server is running on port 9080");
